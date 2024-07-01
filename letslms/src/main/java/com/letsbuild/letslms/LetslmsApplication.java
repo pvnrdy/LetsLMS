5d1c79ab37afa1434d00bf603e5ce15bd91f2780
pvnrdy <pavanreddy0611@gmail.com>
2024-07-01 22:02:48 +0530

    First commit

---
+++ letslms/src/main/java/com/letsbuild/letslms/LetslmsApplication.java
@@ -0,0 +1,12 @@
+package com.letsbuild.letslms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetslmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetslmsApplication.class, args);
+	}	
+}
---
+++ letslms/src/test/java/com/letsbuild/letslms/LetslmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsbuild.letslms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetslmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
